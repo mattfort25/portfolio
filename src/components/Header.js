744cd3f48060d847fa6001da923ca03b4f9a62de
Mahdi <mzangeneh@fortitudenorth.com>
2025-08-12 19:42:38 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.leftSection}>
+        <div className={styles.logo}>
+          <Link href="/" className={styles.logoLink}>
+            Financial Profile
+          </Link>
+        </div>
+      </div>
+
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li>
+            <Link href="/dashboard" className={styles.navLink}>
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link href="/market" className={styles.navLink}>
+              Market
+            </Link>
+          </li>
+          <li>
+            <Link href="/watchlist" className={styles.navLink}>
+              Watchlist
+            </Link>
+          </li>
+          <li>
+            <Link href="/company-list" className={styles.navLink}>
+              Company List
+            </Link>
+          </li>
+          <li>
+            <Link href="/settings" className={styles.navLink}>
+              Settings
+            </Link>
+          </li>
+          <li>
+            <Link href="/about-us" className={styles.navLink}>
+              About Us
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.rightSection}>
+        <Link href="/login" className={styles.loginButton}>
+          Log In
+        </Link>
+        <Link href="/signup" className={styles.signupButton}>
+          Sign Up
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
