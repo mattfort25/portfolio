1cfff193c693b44912ed9d63f3c61a4281697079
Mahdi <mzangeneh@fortitudenorth.com>
2025-09-13 18:06:56 -0400

    changes on database config

--- server/config/db.js
+++ server/config/db.js
@@ -2,27 +2,42 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-// sequelize to point to postgres database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: 5432,
-    dialectOptions: {
-      ssl: { rejectUnauthorized: false },
-    },
-    logging: console.log,
-  }
-);
+const dbUrl = process.env.DATABASE_URL;
+
+const sequelize = dbUrl
+  ? new Sequelize(dbUrl, {
+      dialect: "postgres",
+      // SSL is required for Neon database connections.
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: console.log,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        port: 5432,
+        dialectOptions: {
+          ssl: { rejectUnauthorized: false },
+        },
+        logging: console.log,
+      }
+    );
 
 // test the database connection
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection to database has been established successfully.");
+    console.log(
+      "Connection to the database has been established successfully."
+    );
   } catch (error) {
     console.error("Unable to connect to the database:", error);
     process.exit(1);
